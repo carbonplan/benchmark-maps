5957ce90cb9699ab61299d2db44fd70974f03a18
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-06-05 11:02:21 -0600

    add frame_durations_in_ms

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import datetime
-import itertools
 import json
 import pathlib
 
+import numpy as np
 from playwright.sync_api import sync_playwright
 
 data_dir = pathlib.Path(__file__).parent / 'data'
@@ -12,7 +12,7 @@ data_dir.mkdir(exist_ok=True, parents=True)
 all_data = []
 
 
-def run(playwright, operation, level):
+def run(playwright):
     browser = playwright.chromium.launch()
     context = browser.new_context()
     page = context.new_page()
@@ -49,47 +49,67 @@ def run(playwright, operation, level):
         """
         window._frameCounter = 0;
         window._timerStart = performance.now();
+        window._frameDurations = [];
+        window._prevFrameTime = performance.now();
         window._rafId = requestAnimationFrame(function countFrames() {
+            const currentTime = performance.now();
+            window._frameDurations.push(currentTime - window._prevFrameTime);
+            window._prevFrameTime = currentTime;
             window._frameCounter++;
             window._rafId = requestAnimationFrame(countFrames);
         });
     """
     )
 
-    # Perform operation
-    for _ in range(level):
-        if operation == 'zoom_in':
-            page.keyboard.press('=')
-        elif operation == 'zoom_out':
-            page.keyboard.press('-')
-        # Add more elif conditions here for other operations
-
+    # Wait for the FPS to become static.
+    # page.wait_for_function(
+    #     """
+    #     () => {
+    #         const durationInSeconds = (performance.now() - window._timerStart) / 1000;
+    #         const fps = window._frameCounter / durationInSeconds;
+    #         return fps < 0.1;  // Change this threshold as needed
+    #     }
+    #     """
+    # )
     page.wait_for_load_state('networkidle')
 
-    # Cancel the frame counting and calculate FPS
-    fps = page.evaluate(
+    # Cancel the frame counting and calculate FPS and frame durations
+    durations = page.evaluate(
         """
         cancelAnimationFrame(window._rafId);
         const durationInSeconds = (performance.now() - window._timerStart) / 1000;
         const fps = window._frameCounter / durationInSeconds;
-        fps;
+        const frameDurations = window._frameDurations;
+        [fps, frameDurations];
     """
     )
 
+    fps = durations[0]
+    frame_durations = durations[1]
+
     browser.close()
+
+    # record frame duration and fps
 
     data = {
         'average_fps': round(fps, 0),
+        'frame_durations_in_ms': frame_durations,
         'request_data': request_data,
-        'operation': operation,
-        'zoom_level': level,
     }
 
     all_data.append(data)
 
-    print(
-        f"Average FPS during {data['operation']} operation at level {data['zoom_level']}: {data['average_fps']}"
-    )
+    frame_durations_mean = np.mean(frame_durations)
+    frame_durations_median = np.median(frame_durations)
+    frame_durations_percentiles = np.percentile(frame_durations, [25, 50, 75, 90])
+
+    print(f"Average FPS: {data['average_fps']}")
+    print(f'Average frame duration: {frame_durations_mean}')
+    print(f'Median frame duration: {frame_durations_median}')
+    print(f'25th percentile frame duration: {frame_durations_percentiles[0]}')
+    print(f'50th percentile frame duration: {frame_durations_percentiles[1]}')
+    print(f'75th percentile frame duration: {frame_durations_percentiles[2]}')
+    print(f'90th percentile frame duration: {frame_durations_percentiles[3]}')
     print(f"Total number of requests: {len(data['request_data'])}")
     print(
         f"Average response time for each tile: {sum(x['total_response_time_in_ms'] for x in data['request_data']) / len(data['request_data']):.2f} ms"
@@ -97,12 +117,9 @@ def run(playwright, operation, level):
 
 
 with sync_playwright() as playwright:
-    operations = ['zoom_in', 'zoom_out']  # Add more operations here
-    levels = [1, 2, 3]  # Add more levels here
-    for operation, level in itertools.product(operations, levels):
-        run(playwright, operation, level)
+    run(playwright)
 
     # Write the data to a json file
     now = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%dT%H-%M-%S')
-    with open(data_dir / f'data_{now}.json', 'w') as outfile:
+    with open(data_dir / 'data.json', 'w') as outfile:
         json.dump(all_data, outfile, indent=4, sort_keys=True)
