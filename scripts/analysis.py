37b1cf71d1f61344ac211b8ba6f31ed1bcfb0d6d
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-07-25 21:50:55 -0400

    Separate requests extraction into post-processing

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,53 +1,61 @@
 import argparse
+import json
 
 import holoviews as hv
 import hvplot.pandas
-import numpy as np
 import pandas as pd
 
 pd.options.plotting.backend = 'holoviews'
 
 
-def extract_frames(df: pd.DataFrame, run: int):
+def extract_request_data(*, trace_events, url_filter: str = None):
     """
-    Extract frame information from trace JSON for a given run
+    Extract request data from a list of Chromium trace events, optionally filtering by URL.
+
+    Parameters
+    ----------
+
+    trace_events: list
+         The list of trace events.
+    url_filter : str, optional
+        If specified, only include requests where the URL contains this string.
+
+    Returns
+    -------
+    request_data : list of dictionaries, each containing information about a request.
+        A list of dictionaries, each containing information about a request.
     """
-    return pd.DataFrame(
+    send_requests = pd.json_normalize(
+        [event for event in trace_events if event['name'] == 'ResourceSendRequest']
+    )
+    finish_requests = pd.json_normalize(
+        [event for event in trace_events if event['name'] == 'ResourceFinish']
+    )
+    data = send_requests.merge(
+        finish_requests, on='args.data.requestId', how='left', suffixes=('_send', '_finish')
+    )[
+        [
+            'ts_send',
+            'ts_finish',
+            'args.data.encodedDataLength',
+            'args.data.priority',
+            'args.data.url',
+            'args.data.requestMethod',
+        ]
+    ].rename(
         {
-            'timestamp_ms': np.array(df.iloc[run]['chromium_trace_frame_timestamps_in_micros'])
-            * 1e-3,  # nb: based on timestamps being in nanoseconds
-            'durations_ms': np.array(df.iloc[run]['chromium_trace_frame_durations_in_s']) * 1000,
-            'fps': np.array(df.iloc[run]['chromium_trace_fps']),
-        }
+            'ts_send': 'request_start',
+            'ts_finish': 'response_end',
+            'args.data.encodedDataLength': 'encoded_data_length',
+            'args.data.url': 'url',
+            'args.data.requestMethod': 'method',
+        },
+        axis=1,
     )
-
-
-def extract_requests(df: pd.DataFrame, run: int):
-    """
-    Extract request information from trace JSON for a given run
-    """
-    return pd.DataFrame(df.iloc[run]['chromium_trace_request_data'])
-
-
-def plot_frame_hist(df: pd.DataFrame):
-    """
-    Plot a histogram of frame durations
-    """
-    hist = df.hvplot.hist('durations_ms', bins=100)
-    hist.opts(width=1000, xlabel='Duration (ms)', ylabel='Counts')
-    return hist
-
-
-def plot_frame_durations(df: pd.DataFrame):
-    """
-    Plot rectangles showing each frame duration
-    """
-    df['rectangle'] = df.apply(
-        lambda x: (x['timestamp_ms'], 0, x['timestamp_ms'] + x['durations_ms'], 1), axis=1
-    )
-    boxes = hv.Rectangles(df['rectangle'].to_list())
-    boxes.opts(width=1000, color='lightgreen', xlabel='Timestamp', yaxis=None)
-    return boxes
+    data['request_start'] = data['request_start'] * 1e-3
+    data['response_end'] = data['response_end'] * 1e-3
+    data['total_response_time_ms'] = data['response_end'] - data['request_start']
+    return data[data['url'].str.contains(url_filter)]
 
 
 def plot_requests(df: pd.DataFrame):
@@ -69,13 +77,13 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str)
     parser.add_argument('--run', type=int, default=0)
     args = parser.parse_args()
-    # Extract data
-    df = pd.read_json(args.data)
-    frames = extract_frames(df, args.run)
-    requests = extract_requests(df, args.run)
-    # Create plots
-    hist_plt = plot_frame_hist(frames)
-    frames_plt = plot_frame_durations(frames)
-    requests_plt = plot_requests(requests)
+    # Extract request data
+    with open(args.data) as f:
+        trace_data = json.load(f)
+    url_filter = 'carbonplan-maps.s3.us-west-2.amazonaws.com/v2/demo'
+    filtered_request_data = extract_request_data(
+        trace_events=trace_data['traceEvents'], url_filter=url_filter
+    )
+    requests_plt = plot_requests(filtered_request_data)
     # Show plot using bokeh server
-    hvplot.show((hist_plt + requests_plt + frames_plt).cols(1))
+    hvplot.show(requests_plt)
