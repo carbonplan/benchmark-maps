854e1477d66971981c287d551f2a2eea8c7d6d81
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-07-26 22:04:46 +0000

    Update pyramid pipeline

--- stores/utils.py
+++ stores/utils.py
@@ -1,11 +1,10 @@
+import rioxarray  # noqa
 import xarray as xr
-from cmip6_downscaling.methods.common.utils import validate_zarr_store
-from ndpyramid import pyramid_regrid
+from carbonplan_data.utils import set_zarr_encoding as set_web_zarr_encoding
+from ndpyramid import pyramid_reproject
 
 
-def calc_chunk_dict(
-    ds: xr.Dataset | xr.DataArray, target_mb: int, pixels_per_tile: int = None
-) -> dict:
+def calc_chunk_dict(ds: xr.Dataset, target_mb: int, pixels_per_tile: int) -> dict:
     """Calculate chunks sizes for uncompressed chunks to match target size.
 
     Parameters
@@ -21,24 +20,22 @@ def calc_chunk_dict(
     -------
     target_chunks : dict
     """
-    if isinstance(ds, xr.Dataset):
-        for var in ds.data_vars:
-            data_bytesize = ds[var].dtype.itemsize
-    else:
-        data_bytesize = ds.dtype.itemsize
+    data_bytesize = next(iter(ds.data_vars.values())).dtype.itemsize
     if pixels_per_tile is None:
         slice_mb = ds.nbytes * 1e-6
         target_chunks = {'time': int(target_mb // slice_mb)}
     else:
         slice_mb = data_bytesize * pixels_per_tile * pixels_per_tile * 1e-6
         target_chunks = {'time': int(target_mb // slice_mb)}
+        if target_chunks['time'] > ds.sizes['time']:
+            target_chunks['time'] = ds.sizes['time']
         target_chunks['x'] = pixels_per_tile
         target_chunks['y'] = pixels_per_tile
     return target_chunks
 
 
 def pyramid(
-    ds_path: str, target: str, levels: int = 2, pixels_per_tile: int = 128, target_mb: int = 5
+    ds_path: str, *, target: str, levels: int = 2, pixels_per_tile: int = 128, target_mb: int = 5
 ) -> str:
     '''Create a data pyramid from an xarray Dataset
 
@@ -52,32 +49,39 @@ def pyramid(
         Path to write output data pyamid to
     levels : int, optional
         Number of levels in pyramid, by default 2
+    pixels_per_tile : int, optional
+        Number of pixels along x and y per tile, by default 128
+    target_mb : int, optional
+        Target size in MB for each chunk; used to define chunking along time dimension, by default 5 MB
 
     Returns
     -------
     target : str
     '''
 
-    ds = xr.open_zarr(ds_path)
+    ds = xr.open_zarr(ds_path, chunks={}).rio.write_crs('EPSG:4326')
 
     chunks = calc_chunk_dict(ds, target_mb=target_mb, pixels_per_tile=pixels_per_tile)
 
     other_chunks = {'time': chunks['time']}
+    print(f'creating pyramids from {ds_path}...')
 
     # create pyramid
-    dta = pyramid_regrid(
+    dta = pyramid_reproject(
         ds,
         levels=levels,
         pixels_per_tile=pixels_per_tile,
         other_chunks=other_chunks,
-        regridder_kws={'ignore_degenerate': True},
     )
 
-    # write to target
-    for child in dta.children.values():
-        for variable in child.ds.data_vars:
-            child[variable].encoding['write_empty_chunks'] = True
+    print('setting metadata...')
+    # set encoding
+    for child in dta.children:
+        dta[child].ds = set_web_zarr_encoding(
+            dta[child].ds, codec_config={'id': 'gzip', 'level': 1}, float_dtype='float32'
+        )
 
+    # write to zarr
+    print(f'writing to {target}...')
     dta.to_zarr(target, mode='w')
-    validate_zarr_store(target)
     return target
