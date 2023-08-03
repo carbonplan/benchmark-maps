7aa49d20d6827fcd9d12517e24a024e0baa3cdb9
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-03 19:38:22 -0400

    Update bash scripts

--- main.sh
+++ main.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 playwright install
-python main.py --dataset 1MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4
+python main.py --dataset 1MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
+python main.py --dataset 5MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
+python main.py --dataset 10MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
+python main.py --dataset 25MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
+python main.py --dataset 1MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
+python main.py --dataset 5MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
+python main.py --dataset 10MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
+python main.py --dataset 25MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
