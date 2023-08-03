32d5d61d80345db4859764a917e3143d7ad2bd40
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-03 17:13:39 -0400

    Refactor

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import upath
 from holoviews import opts
+from parsing import extract_event_type, extract_frame_data, extract_request_data
 
 pd.options.plotting.backend = 'holoviews'
 
@@ -32,65 +33,6 @@ def base64_to_img(base64jpeg):
     return cv.imdecode(arr, cv.IMREAD_COLOR)
 
 
-def process_rendering_events(df):
-    """
-    Process rendering events by adding startTime in ms, normalizing dtypes, and sorting by time.
-
-    Parameters
-    ----------
-
-    df: pd.DataFrame
-        DataFrame containing event information
-
-    Returns
-    -------
-    df : Processed DataFrame containing event information
-    """
-    df['startTime'] = df['ts'] * 1e-3
-    # df['startTimeOffset'] = df['startTime'] - traceStartTime
-    if 'args.frameSeqId' in df.columns:
-        df['args.frameSeqId'] = df['args.frameSeqId'].astype(int)
-    return df.sort_values(by='startTime')
-
-
-def combine_first_two_frames(df):
-    """
-    Combine the first two frames into one frame
-
-    Parameters
-    ----------
-
-    df: pd.DataFrame
-        DataFrame containing event information
-
-    Returns
-    -------
-    df : Processed DataFrame with first two frames combined
-    """
-    df.loc[1, 'startTime_Begin'] = df.loc[0, 'startTime_Draw']
-    df.loc[1, 'startTime_Begin_Diff'] = df.loc[2, 'startTime_Begin'] - df.loc[1, 'startTime_Begin']
-    return df.drop(df.head(1).index)
-
-
-def get_start_time(*, trace_events):
-    """
-    Extract the time of the first event that has a startTime
-
-
-    Parameters
-    ----------
-
-    trace_events: list
-        The list of trace events.
-
-    Returns
-    -------
-    start_time: int
-        First non-zero start time from events
-    """
-    return next((x['ts'] for x in trace_events if x['ts']), None) * 1e-3
-
-
 def calculate_snapshot_rmse(*, trace_events, snapshots):
     """
     Extract screenshots from a list of Chromium trace events.
@@ -112,10 +54,7 @@ def calculate_snapshot_rmse(*, trace_events, snapshots):
     def calculate_rmse(predictions, targets):
         return np.sqrt(np.mean((predictions - targets) ** 2))
 
-    screenshots = pd.json_normalize(
-        [event for event in trace_events if event['name'] == 'Screenshot']
-    )
-    screenshots = process_rendering_events(screenshots)
+    screenshots = extract_event_type(trace_events=trace_events, event_name='Screenshot')
     for action_ind, snapshot_base64 in enumerate(snapshots):
         snapshot = base64_to_img(snapshot_base64)
         var = f'rmse_snapshot_{action_ind}'
@@ -125,156 +64,15 @@ def calculate_snapshot_rmse(*, trace_events, snapshots):
     return screenshots
 
 
-def extract_request_data(*, trace_events, url_filter: str = None):
-    """
-    Extract request data from a list of Chromium trace events, optionally filtering by URL.
-
-    Parameters
-    ----------
-
-    trace_events: list
-        The list of trace events.
-    url_filter : str, optional
-        If specified, only include requests where the URL contains this string.
-
-    Returns
-    -------
-    request_data : DataFrame containing information about requests
-    """
-    send_requests = pd.json_normalize(
-        [event for event in trace_events if event['name'] == 'ResourceSendRequest']
-    )
-    finish_requests = pd.json_normalize(
-        [event for event in trace_events if event['name'] == 'ResourceFinish']
-    )
-    data = send_requests.merge(
-        finish_requests, on='args.data.requestId', how='left', suffixes=('_send', '_finish')
-    )[
-        [
-            'ts_send',
-            'ts_finish',
-            'args.data.encodedDataLength',
-            'args.data.priority',
-            'args.data.url',
-            'args.data.requestMethod',
-        ]
-    ].rename(
-        {
-            'ts_send': 'request_start',
-            'ts_finish': 'response_end',
-            'args.data.encodedDataLength': 'encoded_data_length',
-            'args.data.url': 'url',
-            'args.data.requestMethod': 'method',
-        },
-        axis=1,
-    )
-    data['request_start'] = data['request_start'] * 1e-3
-    data['response_end'] = data['response_end'] * 1e-3
-    data['total_response_time_ms'] = data['response_end'] - data['request_start']
-    if url_filter:
-        data = data[data['url'].str.contains(url_filter)]
-    data = data.reset_index(drop=True)
-    return data
-
-
-def extract_markers(*, trace_events):
-    """
-    Extract markers from a list of Chromium trace events.
-
-    Parameters
-    ----------
-
-    trace_events: list
-        The list of trace events.
-
-    zoom_levels: int
-        Number of zoom levels in the test.
-
-    Returns
-    -------
-    markers : DataFrame containing information about markers
-    """
-    markers = pd.json_normalize([event for event in trace_events if 'benchmark-' in event['name']])
-    markers = process_rendering_events(markers)
-    return markers
-
-
-def extract_frame_data(*, trace_events):
-    """
-    Extract frame data from a list of Chromium trace events.
-
-    Parameters
-    ----------
-
-    trace_events: list
-        The list of trace events.
-
-    Returns
-    -------
-    frame_data : DataFrame containing information about frames
-    """
-    # Fetch events of type 'BeginFrame', 'DrawFrame', and 'DroppedFrame'
-    begin_frame_events = pd.json_normalize(
-        [event for event in trace_events if event['name'] == 'BeginFrame']
-    )
-    draw_frame_events = pd.json_normalize(
-        [event for event in trace_events if event['name'] == 'DrawFrame']
-    )
-    dropped_frame_events = pd.json_normalize(
-        [event for event in trace_events if event['name'] == 'DroppedFrame']
-    )
-    commit_events = pd.json_normalize(
-        [event for event in trace_events if event['name'] == 'Commit']
-    ).dropna(subset=['args.frameSeqId'])
-    # Drop duplicates
-    begin_frame_events.drop_duplicates(subset=['args.frameSeqId'], inplace=True)
-
-    for df in [begin_frame_events, draw_frame_events, dropped_frame_events, commit_events]:
-        process_rendering_events(df)
-
-    frame_events = (
-        pd.concat([draw_frame_events, dropped_frame_events])
-        .sort_values(by='startTime')
-        .merge(begin_frame_events, on='args.frameSeqId', suffixes=('_Draw', '_Begin'))
-        .merge(commit_events, on='args.frameSeqId', suffixes=('', '_Commit'), how='left')[
-            [
-                'name_Draw',
-                'name_Begin',
-                'args.frameSeqId',
-                'pid_Draw',
-                'pid_Begin',
-                'startTime_Draw',
-                'startTime_Begin',
-                'startTime',
-            ]
-        ]
-        .rename({'startTime': 'startTime_Commit'}, axis=1)
-    )
-    frame_events['commit_before_draw'] = (
-        frame_events['startTime_Commit'] < frame_events['startTime_Draw']
-    )
-    frame_events = frame_events.drop_duplicates(subset=['name_Begin', 'startTime_Begin'])
-    frame_events['duration'] = -frame_events['startTime_Begin'].diff(periods=-1)
-    frame_events = combine_first_two_frames(frame_events)
-    frame_events['endTime'] = frame_events['startTime_Begin'].shift(periods=-1)
-    frame_events = frame_events.drop(frame_events.tail(1).index)
-    frame_events = frame_events.rename({'startTime_Begin': 'startTime'}, axis=1)
-    frame_events['dropped'] = frame_events['name_Draw'] == 'DroppedFrame'
-    frame_events['isPartial'] = False
-    frame_events['drawn'] = ~frame_events['dropped']
-    frame_events['idle'] = False
-    return frame_events
-
-
-def plot_requests(df: pd.DataFrame, start_time: float):
+def plot_requests(df: pd.DataFrame):
     """
     Plot rectangles showing each request duration
     """
     df['rectangle'] = df.apply(
         lambda x: (
-            x['request_start'] - start_time,
+            x['request_start'],
             x.name,
-            x['response_end'] - start_time,
+            x['response_end'],
             x.name + 1,
         ),
         axis=1,
@@ -284,13 +82,11 @@ def plot_requests(df: pd.DataFrame, start_time: float):
     return boxes
 
 
-def plot_frames(df: pd.DataFrame, start_time: float, *, yl: int = 1):
+def plot_frames(df: pd.DataFrame, *, yl: int = 1):
     """
     Plot rectangles showing each fra,e duration
     """
-    df['rectangle'] = df.apply(
-        lambda x: (x['startTime'] - start_time, yl, x['endTime'] - start_time, yl + 1), axis=1
-    )
+    df['rectangle'] = df.apply(lambda x: (x['startTime'], yl, x['endTime'], yl + 1), axis=1)
     plt_opts = {'width': 1000, 'xlabel': 'Time (ms)', 'yaxis': None, 'title': 'Frames'}
     subsets = {'idle': 'lightgrey', 'isPartial': 'yellow', 'dropped': 'red', 'drawn': 'lightgreen'}
     plots = {}
@@ -301,9 +97,7 @@ def plot_frames(df: pd.DataFrame, start_time: float, *, yl: int = 1):
     return plots['idle'] * plots['isPartial'] * plots['dropped'] * plots['drawn']
 
 
-def plot_screenshot_rmse(
-    df: pd.DataFrame, markers: pd.DataFrame, start_time: float, zoom_level: int
-):
+def plot_screenshot_rmse(df: pd.DataFrame, markers: pd.DataFrame, zoom_level: int):
     """
     Plot difference between the screenshots and baseline frames
     """
@@ -315,31 +109,29 @@ def plot_screenshot_rmse(
         {'color': 'red'},
         {'color': 'purple'},
     ]
-    df['startTime_relative'] = df['startTime'] - start_time
-    markers['startTime_relative'] = markers['startTime'] - start_time
     plt = df.plot.line(
-        x='startTime_relative', y='rmse_snapshot_0', label='zoom 0', color=color_opts[0]['color']
+        x='startTime', y='rmse_snapshot_0', label='zoom 0', color=color_opts[0]['color']
     )
     start_line = hv.VLine(
-        markers[markers['name'] == 'benchmark-initial-load:start'].loc[0, 'startTime_relative']
+        markers[markers['name'] == 'benchmark-initial-load:start'].loc[0, 'startTime']
     )
-    end_line = hv.VLine(df.iloc[df['rmse_snapshot_0'].argmin()]['startTime_relative'])
+    end_line = hv.VLine(df.iloc[df['rmse_snapshot_0'].argmin()]['startTime'])
     plt = (plt * start_line * end_line).opts(opts.VLine(**color_opts[0]))
     for ind in range(1, zoom_level + 1):
         rmse = df.plot.line(
-            x='startTime_relative',
+            x='startTime',
             y=f'rmse_snapshot_{ind}',
             label=f'zoom {ind}',
             color=color_opts[ind]['color'],
         )
         markers[markers['name'] == f'benchmark-zoom_in-level-{ind-1}:start']
-        df.iloc[df[f'rmse_snapshot_{ind-1}'].argmin()]['startTime_relative']
+        df.iloc[df[f'rmse_snapshot_{ind-1}'].argmin()]['startTime']
         start_line = hv.VLine(
             markers[markers['name'] == f'benchmark-zoom_in-level-{ind-1}:start'].iloc[0][
-                'startTime_relative'
+                'startTime'
             ]
         )
-        end_line = hv.VLine(df.iloc[df[f'rmse_snapshot_{ind}'].argmin()]['startTime_relative'])
+        end_line = hv.VLine(df.iloc[df[f'rmse_snapshot_{ind}'].argmin()]['startTime'])
         plt = plt * rmse * (start_line * end_line).opts(opts.VLine(**color_opts[ind]))
     return plt.opts(**plt_opts)
 
@@ -361,10 +153,8 @@ if __name__ == '__main__':
     approach, zarr_version, dataset = metadata['url'].split('/')[-3:]
     # Load trace events
     trace_events = json.loads(upath.UPath(metadata['trace_path']).read_text())['traceEvents']
-    # Get start time
-    start_time = get_start_time(trace_events=trace_events)
     # Get markers
-    markers = extract_markers(trace_events=trace_events)
+    markers = extract_event_type(trace_events=trace_events, event_name='benchmark-', exact=False)
     # Extract request data
     url_filter = 'carbonplan-benchmarks.s3.us-west-2.amazonaws.com/data/'
     filtered_request_data = extract_request_data(trace_events=trace_events, url_filter=url_filter)
@@ -376,10 +166,8 @@ if __name__ == '__main__':
     ]
     screenshot_data = calculate_snapshot_rmse(trace_events=trace_events, snapshots=snapshots)
     # # Create plots
-    requests_plt = plot_requests(filtered_request_data, start_time)
-    frames_plt = plot_frames(filtered_frames_data, start_time, yl=2.5)
-    rmse_plt = plot_screenshot_rmse(
-        screenshot_data, markers, start_time, zoom_level=metadata['zoom_level']
-    )
+    requests_plt = plot_requests(filtered_request_data)
+    frames_plt = plot_frames(filtered_frames_data, yl=2.5)
+    rmse_plt = plot_screenshot_rmse(screenshot_data, markers, zoom_level=metadata['zoom_level'])
     # # Show plot using bokeh server
     hvplot.show((requests_plt + frames_plt + rmse_plt).cols(1))
