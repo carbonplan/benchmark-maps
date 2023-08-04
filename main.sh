27db0bfb8e39ed7c979f734a085f15354279a590
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-04 08:25:24 -0400

    Add detect-provider flag
    Co-authored-by: Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>

--- main.sh
+++ main.sh
@@ -3,11 +3,11 @@ set -e
 
 
 playwright install
-python main.py --dataset 1MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
-python main.py --dataset 5MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
-python main.py --dataset 10MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
-python main.py --dataset 25MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
-python main.py --dataset 1MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
-python main.py --dataset 5MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
-python main.py --dataset 10MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
-python main.py --dataset 25MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks
+python main.py --dataset 1MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks --detect-provider
+python main.py --dataset 5MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks --detect-provider
+python main.py --dataset 10MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks --detect-provider
+python main.py --dataset 25MB-chunks --zarr-version v2 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks --detect-provider
+python main.py --dataset 1MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks --detect-provider
+python main.py --dataset 5MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks --detect-provider
+python main.py --dataset 10MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks --detect-provider
+python main.py --dataset 25MB-chunks --zarr-version v3 --action zoom_in --zoom-level 4 --s3-bucket s3://carbonplan-benchmarks --detect-provider
