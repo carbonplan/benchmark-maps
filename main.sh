482049d6aab92bd25cd66354fea69911d81981b7
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-06 15:28:00 -0400

    Variable timeout limit

--- main.sh
+++ main.sh
@@ -2,15 +2,16 @@
 set -e
 
 playwright install
-datasets=("1MB-chunks" "5MB-chunks" "10MB-chunks" "25MB-chunks")
 versions=("v2" "v3")
-timeout="60000"
 action="--action zoom_in --zoom-level 3"
 
 for version in "${versions[@]}"
 do
-  for dataset in "${datasets[@]}"
+  for chunk in 1 5 10 25
   do
+    timeout=$((c=5000, chunk, y=2000, c+chunk*y))
+    dataset="${chunk}MB-chunks"
+    echo ${dataset}
     python main.py --dataset ${dataset} --zarr-version ${version} --timeout ${timeout} --detect-provider ${action}
   done
 done
