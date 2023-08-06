f5d276872a9f3e5aa55cdf9fd69dba52b9ad3030
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-05 21:29:41 -0400

    Remove number of zooms

--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@ set -e
 
 
 playwright install
-datasets=("1MB-chunks" "5MB-chunks" "10MB-chunks" "25MB-chunks")
+datasets=("25MB-chunks")
 versions=("v2" "v3")
 
 for version in "${versions[@]}"
 do
   for dataset in "${datasets[@]}"
   do
-    python main.py --dataset ${dataset} --zarr-version ${version} --action zoom_in --zoom-level 4 --detect-provider --s3-bucket s3://carbonplan-benchmarks
+    python main.py --dataset ${dataset} --zarr-version ${version} --action zoom_in --zoom-level 3 --detect-provider --s3-bucket s3://carbonplan-benchmarks
   done
 done
