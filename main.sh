75a67455ce72584a1d735b30aaf81dc5d4914879
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-06 17:32:37 -0400

    Run on subset of v2

--- main.sh
+++ main.sh
@@ -2,17 +2,17 @@
 set -e
 
 playwright install
-versions=("v2" "v3")
+versions=("v2")
 action=""
 runs=100
 
 for version in "${versions[@]}"
 do
-  for chunk in 1 5 10 25
+  for chunk in 5 10 25
   do
     timeout=$((c=5000, chunk, y=2000, c+chunk*y))
     dataset="${chunk}MB-chunks"
     echo ${dataset}
-    python main.py --dataset ${dataset} --zarr-version ${version} --timeout ${timeout} --detect-provider ${action} --runs 100
+    python main.py --dataset ${dataset} --zarr-version ${version} --timeout ${timeout} --detect-provider ${action} --runs ${runs}
   done
 done
