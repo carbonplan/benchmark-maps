207af7619cde7784eaeab148155f0f136fae95ce
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-06 16:16:14 -0400

    Multiple runs without zoom

--- main.sh
+++ main.sh
@@ -3,7 +3,8 @@ set -e
 
 playwright install
 versions=("v2" "v3")
-action="--action zoom_in --zoom-level 3"
+action=""
+runs=100
 
 for version in "${versions[@]}"
 do
@@ -12,6 +13,6 @@ do
     timeout=$((c=5000, chunk, y=2000, c+chunk*y))
     dataset="${chunk}MB-chunks"
     echo ${dataset}
-    python main.py --dataset ${dataset} --zarr-version ${version} --timeout ${timeout} --detect-provider ${action}
+    python main.py --dataset ${dataset} --zarr-version ${version} --timeout ${timeout} --detect-provider ${action} --runs 100
   done
 done
