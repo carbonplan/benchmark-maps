e7bf9ae9ff57041165135cb17c740849614986b4
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-06 12:50:13 -0400

    Update main script for baseline image generation

--- main.sh
+++ main.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-
 playwright install
 datasets=("1MB-chunks" "5MB-chunks" "10MB-chunks" "25MB-chunks")
 versions=("v2" "v3")
+timeout="60000"
+action="--action zoom_in --zoom-level 3"
 
 for version in "${versions[@]}"
 do
   for dataset in "${datasets[@]}"
   do
-    python main.py --dataset ${dataset} --zarr-version ${version} --action zoom_in --zoom-level 3 --detect-provider --timeout 60000
+    python main.py --dataset ${dataset} --zarr-version ${version} --timeout ${timeout} --detect-provider ${action}
   done
 done
