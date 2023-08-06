45133a90683d71e4ba625044bb6b2794c2037939
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-05 21:31:06 -0400

    Long timeout

--- main.sh
+++ main.sh
@@ -10,6 +10,6 @@ for version in "${versions[@]}"
 do
   for dataset in "${datasets[@]}"
   do
-    python main.py --dataset ${dataset} --zarr-version ${version} --action zoom_in --zoom-level 3 --detect-provider
+    python main.py --dataset ${dataset} --zarr-version ${version} --action zoom_in --zoom-level 3 --detect-provider --timeout 60000
   done
 done
