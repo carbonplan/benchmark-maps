d0b5134a61eddbb2b332c11aae95fdd1f48005c8
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-17 20:22:47 -0400

    Make test headless

--- carbonplan_benchmarks/tests/test_entrypoint.py
+++ carbonplan_benchmarks/tests/test_entrypoint.py
@@ -2,7 +2,5 @@ import os
 
 
 def test_entrypoint():
-    exit_status = os.system(
-        'carbonplan_benchmarks --dataset pyramids-v3-sharded-4326-1MB --non-headless'
-    )
+    exit_status = os.system('carbonplan_benchmarks --dataset pyramids-v3-sharded-4326-1MB')
     assert exit_status == 0
