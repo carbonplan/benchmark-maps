2d624f26e9c4ff84f9abc91d5b3c44bd92108284
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-08-20 21:54:49 -0400

    Update test_entrypoint.py

--- carbonplan_benchmarks/tests/test_entrypoint.py
+++ carbonplan_benchmarks/tests/test_entrypoint.py
@@ -2,5 +2,5 @@ import os
 
 
 def test_entrypoint():
-    exit_status = os.system('carbonplan_benchmarks --dataset pyramids-v3-sharded-4326-1MB')
+    exit_status = os.system('carbonplan_benchmarks --dataset pyramids-v2-3857-True-128-1-0-0-f4-0-0-0-gzipL1-100')
     assert exit_status == 0
